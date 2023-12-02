4ff21ca42305077eebd478e40358a85605261ed9
Loi Tran <primetimetran@gmail.com>
2023-12-01 21:35:07 -0500

    1.0.10

--- dist/Turboship/Framework.js
+++ dist/Turboship/Framework.js
@@ -1,106 +1,140 @@
-"use strict";
+'use strict'
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.frameworkMap = exports.default = void 0;
-var _fs = _interopRequireDefault(require("fs"));
-var _path = _interopRequireDefault(require("path"));
-var _url = require("url");
-var _Generator = _interopRequireDefault(require("./Generator.js"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-const _filename = (0, _url.fileURLToPath)(import.meta.url);
-const _dirname = _path.default.dirname(_filename);
+Object.defineProperty(exports, '__esModule', {
+  value: true,
+})
+exports.frameworkMap = exports.default = void 0
+var _fs = _interopRequireDefault(require('fs'))
+var _path = _interopRequireDefault(require('path'))
+var _url = require('url')
+var _Generator = _interopRequireDefault(require('./Generator.js'))
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj }
+}
+const _filename = (0, _url.fileURLToPath)(import.meta.url)
+const _dirname = _path.default.dirname(_filename)
 class Framework {
   constructor(name, options, entities, zip) {
-    this.name = name;
-    this.framework = frameworkMap[name];
-    this.zip = zip;
-    this.entities = entities;
-    this.options = options;
-    this.options.frameworkName = name;
+    this.name = name
+    this.framework = frameworkMap[name]
+    this.zip = zip
+    this.entities = entities
+    this.options = options
+    this.options.frameworkName = name
   }
   createDirectories() {
-    this.framework.rootDirectories.forEach(dir => {
-      this.zip.folder(`${this.name}/${dir}`);
-    });
-    return this.zip;
+    this.framework.rootDirectories.forEach((dir) => {
+      this.zip.folder(`${this.name}/${dir}`)
+    })
+    return this.zip
   }
   zipBaseDirectory() {
-    const basePath = `${_dirname}/nuxt`;
-    getZippedFolderSync(basePath, this.zip);
-    return this.zip;
+    const basePath = `${_dirname}/nuxt`
+    getZippedFolderSync(basePath, this.zip)
+    return this.zip
   }
   async build() {
-    const entities = Object.values(this.entities);
-    const generator = await new _Generator.default(entities, this.options, this.zip);
-    return await generator.buildGenesis();
+    const entities = Object.values(this.entities)
+    const generator = await new _Generator.default(
+      entities,
+      this.options,
+      this.zip
+    )
+    return await generator.buildGenesis()
   }
   setup() {
-    const framework = frameworkMap[this.name];
-    const root = '/tmp/turboship/' + framework.name;
+    const framework = frameworkMap[this.name]
+    const root = '/tmp/turboship/' + framework.name
     try {
       _fs.default.mkdirSync(root, {
-        recursive: true
-      });
-      framework.rootDirectories.forEach(dir => {
-        const fullPath = `${root}/${dir}`;
+        recursive: true,
+      })
+      framework.rootDirectories.forEach((dir) => {
+        const fullPath = `${root}/${dir}`
         try {
           _fs.default.mkdirSync(fullPath, {
-            recursive: true
-          });
-          log('Create: ', fullPath, 'yellow');
+            recursive: true,
+          })
+          log('Create: ', fullPath, 'yellow')
         } catch (err) {}
-      });
+      })
     } catch (err) {}
   }
 }
-exports.default = Framework;
+exports.default = Framework
 function getZippedFolderSync(dir, zip) {
-  let allPaths = getFilePathsRecursiveSync(dir);
+  let allPaths = getFilePathsRecursiveSync(dir)
   zip.sync(() => {
     for (let filePath of allPaths) {
-      let addPath = _path.default.relative(_path.default.join(dir, '..'), filePath);
-      let data = _fs.default.readFileSync(filePath);
-      zip.file(addPath, data);
+      let addPath = _path.default.relative(
+        _path.default.join(dir, '..'),
+        filePath
+      )
+      let data = _fs.default.readFileSync(filePath)
+      zip.file(addPath, data)
     }
-  });
+  })
 }
 function getFilePathsRecursiveSync(dir) {
-  var results = [];
-  let list = _fs.default.readdirSync(dir);
-  var pending = list.length;
-  if (!pending) return results;
+  console.log({
+    dir,
+  })
+  var results = []
+  if (!dir) return results
+  let list = _fs.default.readdirSync(dir)
+  var pending = list.length
+  if (!pending) return results
   for (let file of list) {
-    file = _path.default.resolve(dir, file);
-    let stat = _fs.default.statSync(file);
+    file = _path.default.resolve(dir, file)
+    let stat = _fs.default.statSync(file)
     if (stat && stat.isDirectory()) {
-      let res = getFilePathsRecursiveSync(file);
-      results = results.concat(res);
+      let res = getFilePathsRecursiveSync(file)
+      results = results.concat(res)
     } else {
-      results.push(file);
+      results.push(file)
     }
-    if (! --pending) return results;
+    if (!--pending) return results
   }
-  return results;
+  return results
 }
-const frameworkMap = exports.frameworkMap = {
+const frameworkMap = (exports.frameworkMap = {
   nuxt: {
     name: 'nuxt',
     version: '3.8.0',
-    rootDirectories: ['components', 'composables', 'content', 'layouts', 'pages', 'plugins', 'server', 'utils',
-    // Must be lowercase
-    'server/models', 'server/API',
-    // Must be plural
-    'server/Utils', 'components/Admin', 'components/The/Navbar'],
+    rootDirectories: [
+      'components',
+      'composables',
+      'content',
+      'layouts',
+      'pages',
+      'plugins',
+      'server',
+      'utils',
+      // Must be lowercase
+      'server/models',
+      'server/API',
+      // Must be plural
+      'server/Utils',
+      'components/Admin',
+      'components/The/Navbar',
+    ],
     adminUIFiles: ['EntityForm.vue', 'Form.vue', 'Table.vue'],
     adminBuildMethodMap: {
       'Form.vue': 'buildForm',
       'Table.vue': 'buildTable',
-      'EntityForm.vue': 'buildEntityForm'
+      'EntityForm.vue': 'buildEntityForm',
     },
-    adminGlobals: ['./components/Admin/Form/Field.vue', './components/Admin/Form/Pagination.vue'],
-    apiFiles: ['index.get.', 'index.post.', '[_id].delete.', '[_id].get.', '[_id].put.'],
+    adminGlobals: [
+      './components/Admin/Form/Field.vue',
+      './components/Admin/Form/Pagination.vue',
+    ],
+    apiFiles: [
+      'index.get.',
+      'index.post.',
+      '[_id].delete.',
+      '[_id].get.',
+      '[_id].put.',
+    ],
     apiContent: {
       'index.get.': function (label) {
         return `export default defineEventHandler(async (e) => {
@@ -122,7 +156,7 @@ const frameworkMap = exports.frameworkMap = {
           }
           return response
         })
-      `;
+      `
       },
       'index.post.': function (label) {
         return `export default defineEventHandler(async (event) => {
@@ -133,7 +167,7 @@ const frameworkMap = exports.frameworkMap = {
             return error
           }
         })
-      `;
+      `
       },
       '[_id].delete.': function (label) {
         return ` export default defineEventHandler(async (event) => {
@@ -144,7 +178,7 @@ const frameworkMap = exports.frameworkMap = {
             return error
           }
         })
-      `;
+      `
       },
       '[_id].get.': function (label) {
         return `export default defineEventHandler(async (event) => {
@@ -154,7 +188,7 @@ const frameworkMap = exports.frameworkMap = {
             return error
           }
         })
-      `;
+      `
       },
       '[_id].put.': function (label) {
         return `export default defineEventHandler(async (event) => {
@@ -169,13 +203,13 @@ const frameworkMap = exports.frameworkMap = {
             return error
               }
         })
-      `;
-      }
-    }
+      `
+      },
+    },
   },
   flutter: {
     name: 'flutter',
     version: '3.8.0',
-    rootDirectories: ['src']
-  }
-};
+    rootDirectories: ['src'],
+  },
+})
